4d4785e51f2d8a8d78d514e6e246ee64ebc7c470
nandhiniA98 <34852100+nandhiniA98@users.noreply.github.com>
2020-03-26 15:56:01 +0530

    Update Gitclass.java

--- gitbasictest/src/gitbasictest/Gitclass.java
+++ gitbasictest/src/gitbasictest/Gitclass.java
@@ -1,5 +1,21 @@
 package gitbasictest;
 
+import java.util.Scanner;
+
 public class Gitclass {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("haii this is git hub assessment");
+Scanner scn=new Scanner(System.in);
+System.out.println("enter your name");
+String name=scn.next();
+System.out.println("enter your age");
+int age=scn.nextInt();
+System.out.println("Name: "+name);
+System.out.println("Age: "+age);
+	}
 
 }
